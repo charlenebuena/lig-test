ac92ad38500dbae6d0c838f4b8167c3668ce0c77
charlenebuena <charmariebuena@gmail.com>
2019-10-15 21:44:39 +0800

    init commit master

---
+++ lig-frontend/src/app/homepage/homepage.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+    declarations: [
+
+    ],
+    imports: [
+        BrowserModule,
+        SharedModule
+    ],
+    providers: [],
+})
+export class HomepageModule { }
